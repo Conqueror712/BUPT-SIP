86c782cbc380c7874e52cc2264e1890780e3f0a8
imliyk <126547206+imliyk@users.noreply.github.com>
2024-06-07 00:43:12 +0800

    Update hparams.py
    添加注释

--- FinalLab_SpeechSynthesis/vocoder/hparams.py
+++ FinalLab_SpeechSynthesis/vocoder/hparams.py
@@ -1,8 +1,8 @@
 from synthesizer.hparams import hparams as _syn_hp
 
 
-# Audio settings------------------------------------------------------------------------
-# Match the values of the synthesizer
+# 音频设置------------------------------------------------------------------------
+# 匹配语音合成器的值
 sample_rate = _syn_hp.sample_rate
 n_fft = _syn_hp.n_fft
 num_mels = _syn_hp.num_mels
@@ -15,15 +15,14 @@ mel_max_abs_value = _syn_hp.max_abs_value
 preemphasis = _syn_hp.preemphasis
 apply_preemphasis = _syn_hp.preemphasize
 
-bits = 9                            # bit depth of signal
-mu_law = True                       # Recommended to suppress noise if using raw bits in hp.voc_mode
-                                    # below
+bits = 9                            # 信号的比特深度
+mu_law = True                       # 如果使用原始比特在 hp.voc_mode 中，推荐抑制噪声
+                                
 
 
-# WAVERNN / VOCODER --------------------------------------------------------------------------------
-voc_mode = 'RAW'                    # either 'RAW' (softmax on raw bits) or 'MOL' (sample from 
-# mixture of logistics)
-voc_upsample_factors = (5, 5, 8)    # NB - this needs to correctly factorise hop_length
+# WAVERNN / 语音编码器  --------------------------------------------------------------------------------
+voc_mode = 'RAW'                    # 可以是 'RAW' (在原始比特上使用 softmax) 或 'MOL' (从混合逻辑分布中采样)
+voc_upsample_factors = (5, 5, 8)    # 正确分解 hop_length
 voc_rnn_dims = 512
 voc_fc_dims = 512
 voc_compute_dims = 128
@@ -33,12 +32,11 @@ voc_res_blocks = 10
 # Training
 voc_batch_size = 100
 voc_lr = 1e-4
-voc_gen_at_checkpoint = 5           # number of samples to generate at each checkpoint
-voc_pad = 2                         # this will pad the input so that the resnet can 'see' wider 
-                                    # than input length
-voc_seq_len = hop_length * 5        # must be a multiple of hop_length
+voc_gen_at_checkpoint = 5           # 每个检查点生成的样本数量
+voc_pad = 2                         # 这将填充输入，以便 resnet 可以看到比输入长度更宽的范围
+voc_seq_len = hop_length * 5        # 必须是 hop_length 的倍数
 
 # Generating / Synthesizing
-voc_gen_batched = True              # very fast (realtime+) single utterance batched generation
-voc_target = 8000                   # target number of samples to be generated in each batch entry
-voc_overlap = 400                   # number of samples for crossfading between batches
+voc_gen_batched = True              # 非常快（实时+）的单句批处理生成
+voc_target = 8000                   # 每个批处理条目生成的目标样本数
+voc_overlap = 400                   # 批处理之间交叉淡入淡出的样本数
