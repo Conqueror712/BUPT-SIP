b01b13ad3f4bcf497cbd22db1368ff2e9259c350
imliyk <126547206+imliyk@users.noreply.github.com>
2024-06-15 20:31:53 +0800

    Update utterance.py
    添加注释

--- FinalLab_SpeechSynthesis/encoder/data_objects/utterance.py
+++ FinalLab_SpeechSynthesis/encoder/data_objects/utterance.py
@@ -5,17 +5,21 @@ class Utterance:
     def __init__(self, frames_fpath, wave_fpath):
         self.frames_fpath = frames_fpath
         self.wave_fpath = wave_fpath
-        
+
+    #从文件中加载并返回语音帧数据
     def get_frames(self):
         return np.load(self.frames_fpath)
 
     def random_partial(self, n_frames):
         """
-        Crops the frames into a partial utterance of n_frames
+        从完整的语音片段中随机裁剪出一个指定帧数的部分语音片段。
         
-        :param n_frames: The number of frames of the partial utterance
-        :return: the partial utterance frames and a tuple indicating the start and end of the 
-        partial utterance in the complete utterance.
+        参数:
+            n_frames (int): 需要的部分语音片段的帧数。
+        
+        返回值:
+            tuple: 第一个元素为包含指定帧数的部分语音片段的NumPy数组，第二个元素为一个元组，
+                   指明部分语音片段在完整语音片段中的起始和结束帧的索引。
         """
         frames = self.get_frames()
         if frames.shape[0] == n_frames:
